4f023ffc89b67dcc859514540220255a75a2357a
ElJohnnie <johnfollmer1995@gmail.com>
2023-11-02 19:42:20 -0300

    finished location languages config

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,24 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {notFound} from 'next/navigation';
- 
+import { ReactNode } from 'react';
+
 export function generateStaticParams() {
-  return [{locale: 'en'}, {locale: 'de'}];
+  return [{locale: 'en'}, {locale: 'pt'}];
 }
- 
-export default async function LocaleLayout({children, params: {locale}}) {
+
+interface LocaleLayout {
+  children?: ReactNode,
+  params?: any,
+}
+
+export default async function LocaleLayout({children, params: {locale}}: Readonly<LocaleLayout>) {
   let messages;
   try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
+    messages = (await import(`../../locale/${locale}/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <body>
