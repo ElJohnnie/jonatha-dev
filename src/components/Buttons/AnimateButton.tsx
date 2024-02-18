4b52b91bb7869a6bdeb924f24eb7ac57c78c18cb
ElJohnnie <johnfollmer1995@gmail.com>
2024-02-18 11:08:18 -0300

    feat: increment design pattern

--- src/components/Buttons/AnimateButton.tsx
+++ src/components/Buttons/AnimateButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { linkStyleString } from './styles';
+import { linkStyle } from './styles';
 
 interface Props {
   route: string;
@@ -11,7 +11,7 @@ interface Props {
 
 export default function AnimateButton({ route, title, icon }: Readonly<Props>) {
   return (
-    <Link href={route ?? '#'} className={linkStyleString}>
+    <Link href={route ?? '#'} className={linkStyle}>
       <span className='ease absolute left-0 top-0 h-0 w-0 border-t-2 border-indigo-500 transition-all duration-200 group-hover:w-full'></span>
       <span className='ease absolute right-0 top-0 h-0 w-0 border-r-2 border-indigo-500 transition-all duration-200 group-hover:h-full'></span>
       <span className='ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-indigo-500 transition-all duration-200 group-hover:w-full'></span>
--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -1,27 +1,4 @@
-const linkStyle = [
-  'align-center',
-  'group',
-  'relative',
-  'inline-flex',
-  'items-center',
-  'justify-center',
-  'overflow-hidden',
-  'rounded',
-  'border',
-  'border-transparent',
-  'bg-transparent',
-  'px-12',
-  'py-3',
-  'text-sm',
-  'font-bold',
-  'text-slate-200/60',
-  'hover:text-indigo-500',
-  'focus:outline-none',
-  'focus:ring',
-  'active:bg-indigo-600',
-  'active:text-white',
-];
+const linkStyle =
+  'align-center group relative inline-flex items-center justify-center overflow-hidden rounded border border-transparent bg-transparent px-12 py-3 text-sm font-bold text-slate-200/60 hover:text-indigo-500 focus:outline-none focus:ring active:bg-indigo-600 active:text-white';
 
-const linkStyleString = linkStyle.join(' ');
-
-export { linkStyleString };
+export { linkStyle };
