90dbcc654b1330c02186ffec736bcc29d6919920
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-01-26 17:49:43 -0300

    chore: update notion use pattern

---
+++ src/adapters/notion-to-markdown.adapter.ts
@@ -0,0 +1,15 @@
+import { NotionToMarkdown } from 'notion-to-md';
+
+export default class NotionToMarkdownAdapter {
+    private readonly notionToMarkdown: NotionToMarkdown;
+    
+    constructor({ notionClient }: { notionClient: any }) {
+        this.notionToMarkdown = new NotionToMarkdown({ notionClient: notionClient });
+    }
+
+    async pageToMarkdownAndString(pageId: string) {
+        const mdblocks = await this.notionToMarkdown.pageToMarkdown(pageId);
+        return this.notionToMarkdown.toMarkdownString(mdblocks);
+    }
+    
+ }
