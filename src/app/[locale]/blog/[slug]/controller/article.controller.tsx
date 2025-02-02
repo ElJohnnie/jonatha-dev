276e169f0e7f08052eea6463bad52517f7718f2e
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-02-02 12:28:47 -0300

    metadata test

--- src/app/[locale]/blog/[slug]/controller/article.controller.tsx
+++ src/app/[locale]/blog/[slug]/controller/article.controller.tsx
@@ -1,34 +1,9 @@
 import ArticleView from '@/app/[locale]/blog/[slug]/view/article.view';
 import { ArticleProps } from '../../types';
-import type { Metadata } from 'next';
-
-
-export async function generateMetadata({ content }: { content: { title: string } }): Promise<Metadata> {
-  const title = 'Jonatha Dev - Blog';
-  const description = content?.title;
-  const siteName = 'Jonatha Dev';
-
-  return {
-    metadataBase: new URL('https://jonathadev.vercel.app'),
-    title,
-    description,
-    verification: {
-      google: process.env.GOOGLE_SITE_VERIFICATION,
-    },
-    openGraph: {
-      title,
-      description,
-      siteName,
-      type: 'website',
-    },
-  };
-}
-
 
 export default function ArticleController({
   content,
   tagPosts,
 }: Readonly<ArticleProps>) {
-  generateMetadata({ content });
   return <ArticleView content={content} tagPosts={tagPosts} />;
 }
--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,15 +1,48 @@
-import ArticleController from './controller/article.controller';
-import { getPost, getPostsByTag } from '@/services/notion.blog';
+import { Metadata } from "next";
+import ArticleController from "./controller/article.controller";
+import { getPost, getPostsByTag } from "@/services/notion.blog";
 
 interface PageProps {
   params: { slug: string };
   searchParams: { tag: string };
 }
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const post = await getPost(params.slug);
 
-export default async function Page({
-  params,
-  searchParams,
-}: Readonly<PageProps>) {
+  if (!post) {
+    return {
+      title: "Post não encontrado",
+      description: "O post que você está procurando não existe.",
+    };
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      url: `/blog/${params.slug}`,
+      type: "article",
+      images: [
+        {
+          url: "/images/jonatha.png",
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.description,
+      images: ["/images/jonatha.png"],
+    },
+  };
+}
+
+export default async function Page({ params, searchParams }: Readonly<PageProps>) {
   const slug = params.slug;
   const tag = searchParams.tag;
 
