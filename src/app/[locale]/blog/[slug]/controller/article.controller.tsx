2bc33e0f65218fbc0b9c7a7060da5d58f108f147
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-02-02 11:47:34 -0300

    fix: fixed get serverside props on the blog controller

--- src/app/[locale]/blog/[slug]/controller/article.controller.tsx
+++ src/app/[locale]/blog/[slug]/controller/article.controller.tsx
@@ -1,6 +1,4 @@
-import { GetServerSideProps } from 'next';
 import ArticleView from '@/app/[locale]/blog/[slug]/view/article.view';
-import { getPost, getPostsByTag } from '@/services/notion.blog';
 import { ArticleProps } from '../../types';
 
 export default function ArticleController({
@@ -10,19 +8,3 @@ export default function ArticleController({
   return <ArticleView content={content} tagPosts={tagPosts} />;
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { params, query } = context;
-  const slug = params?.slug as string;
-  const tag = query.tag as string;
-
-  const contentPost = getPost(slug);
-  const postsByTag = getPostsByTag(tag, slug);
-  const [content, tagPosts] = await Promise.all([contentPost, postsByTag]);
-
-  return {
-    props: {
-      content,
-      tagPosts,
-    },
-  };
-};
