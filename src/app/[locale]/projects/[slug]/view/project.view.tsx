99100cd13d98da734bb8dbc174ecc26054ccbc59
Jonatha Follmer <johnfollmer1995@gmail.com>
2025-06-03 16:13:26 -0300

    hotfix

--- src/app/[locale]/projects/[slug]/view/project.view.tsx
+++ src/app/[locale]/projects/[slug]/view/project.view.tsx
@@ -12,13 +12,13 @@ export default function ProjectView({
     <section className='mt-4 h-auto pt-4'>
       <div className='mx-auto max-w-screen-sm justify-center md:max-w-screen-2xl'>
         <div className='flex justify-center'>
-          <div className='prose mb-8 max-w-none md:w-3/4'>
+          <div className='prose mb-8 max-w-none w-3/4'>
             <Markdown>{content}</Markdown>
           </div>
         </div>
 
         <div className='flex justify-center'>
-          <div className='mb-8 max-w-none md:w-3/4'>
+          <div className='mb-8 max-w-none w-3/4'>
             <ImageCarousel images={images} />
           </div>
         </div>
